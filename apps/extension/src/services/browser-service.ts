023737bd6ebdad9d4d4b6434ceca10cb2b3b507e
suminhan123 <suminh324@gmail.com>
2026-02-05 23:31:30 +0900

    feat: storage 데이터 Map 형식 변환

--- apps/extension/src/services/browser-service.ts
+++ apps/extension/src/services/browser-service.ts
@@ -1,81 +1,79 @@
-import type { PageSnapshot, StorageSession } from "src/types/storage";
+import {
+  defaultStorage,
+  type PageSnapshot,
+  type StorageSession,
+} from "src/types/storage";
 import { getStorage, setStorage } from "src/utils/storage";
 
 async function getBrowserSession() {
   const storage = await getStorage(["sessions"]);
-  return storage.sessions;
+  return storage.sessions ?? {};
 }
 
-async function setBrowserSession(sessions: StorageSession[]) {
+async function getBrowserSessionById(tabId: string) {
+  const sessions = await getBrowserSession();
+  return sessions[tabId];
+}
+
+async function setBrowserSession(sessions: Record<string, StorageSession>) {
   await setStorage({ sessions });
 }
 
 async function clearBrowserSession() {
-  await setStorage({ sessions: [] });
+  await setStorage(defaultStorage);
+}
+
+async function deleteBrowserSession(tabId: string) {
+  if (!tabId) return;
+  const sessions = await getBrowserSession();
+  console.log("deleteBrowserSession >>>>>>>>>>>>", sessions, tabId);
+  delete sessions[tabId];
+  await setBrowserSession(sessions);
 }
 
 async function addBrowserSession(tabId: string, data: PageSnapshot) {
+  if (!tabId) return;
   const sessions = await getBrowserSession();
-  const newSession: StorageSession = {
+  console.log("addBrowserSession >>>>>>>>>>>>", sessions, tabId, data);
+  sessions[tabId] = {
     ...data,
-    tabId,
     visitedAt: new Date().toISOString(),
     closedAt: null,
   };
-  console.log("addBrowserSession >>>>>>>>>>>>", newSession);
-  await setBrowserSession([...sessions, newSession]);
+
+  await setBrowserSession(sessions);
 }
 
-async function closeBrowserSession(tabId: string) {
+async function closeBrowserSession() {
+  let result = {};
   const sessions = await getBrowserSession();
-  console.log("close sessions", sessions, tabId);
-  if (!sessions.length) return;
-
-  const sessionIdx = sessions.findIndex((session) => session.tabId === tabId);
-  if (sessionIdx < 0) return;
-
-  const updatedSession = [...sessions];
-
-  updatedSession[sessionIdx] = {
-    ...updatedSession[sessionIdx],
-    closedAt: new Date().toISOString(),
-  } as StorageSession;
-
-  console.log("closeBrowserSession >>>>>>>>>>>>", updatedSession);
-  await setBrowserSession(updatedSession);
+  console.log("closeBrowserSession >>>>>>>>>>>>", sessions);
+  for (const session of Object.values(sessions)) {
+    if (session.closedAt === null) {
+      session.closedAt = new Date().toISOString();
+      result = session;
+    }
+  }
+  await setBrowserSession(sessions);
+  return result;
 }
 
-async function addPrevBrowserSession(tabId: string) {
+async function visitBrowserSession(tabId: string) {
   const sessions = await getBrowserSession();
-  if (!sessions.length) return;
-
-  const sessionIdx = sessions.findIndex((session) => session.tabId === tabId);
-  if (sessionIdx < 0) return;
-
-  await addBrowserSession(tabId, sessions[sessionIdx] as PageSnapshot);
-}
-
-async function closeLastBrowserSession() {
-  const sessions = await getBrowserSession();
-  if (!sessions.length) return;
-  const updatedSession = [...sessions];
-
-  updatedSession[updatedSession.length - 1] = {
-    ...updatedSession[updatedSession.length - 1],
-    closedAt: new Date().toISOString(),
-  } as StorageSession;
-
-  console.log("closeLastBrowserSession >>>>>>>>>>>>", updatedSession);
-
-  await setBrowserSession(updatedSession);
+  console.log("visitBrowserSession >>>>>>>>>>>>", sessions, tabId);
+  if (!sessions[tabId]) return;
+  sessions[tabId].visitedAt = new Date().toISOString();
+  sessions[tabId].closedAt = null;
+  await setBrowserSession(sessions);
 }
 
 export {
   addBrowserSession,
-  addPrevBrowserSession,
   clearBrowserSession,
   closeBrowserSession,
-  closeLastBrowserSession,
+  deleteBrowserSession,
   getBrowserSession,
+  getBrowserSessionById,
   setBrowserSession,
+  visitBrowserSession,
 };
