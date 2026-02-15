4d9284e64754c5ff511ffb047e55781c5ed291f3
suminhan123 <suminh324@gmail.com>
2026-02-15 19:31:52 +0900

    feat: 플러그인 ToogleGroup 컴포넌트 구현

---
+++ apps/extension/src/components/ToggleGroup.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import {
+  cn,
+  ToggleGroup as BaseToggleGroup,
+  ToggleGroupItem as BaseToggleGroupItem,
+} from "@recap/ui";
+
+type ToggleGroupProps = React.ComponentPropsWithoutRef<typeof BaseToggleGroup>;
+const ToggleGroup = ({ className, ...props }: ToggleGroupProps) => {
+  return <BaseToggleGroup className={cn(className)} {...props} />;
+};
+
+type ToggleGroupItemProps = React.ComponentPropsWithoutRef<
+  typeof BaseToggleGroupItem
+>;
+const ToggleGroupItem = React.forwardRef<
+  React.ElementRef<typeof BaseToggleGroupItem>,
+  ToggleGroupItemProps
+>(({ className, ...props }, ref) => (
+  <BaseToggleGroupItem
+    ref={ref}
+    className={cn(
+      "text-gray-500 border border-solid border-gray-200 bg-gray-200 text-subtitle-2-rg py-1 px-3",
+      "data-[state=on]:bg-white data-[state=on]:text-gray-900",
+      className,
+    )}
+    {...props}
+  />
+));
+ToggleGroupItem.displayName = "ToggleGroupItem";
+
+export { ToggleGroup, ToggleGroupItem };
