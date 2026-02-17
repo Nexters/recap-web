6b4df954550130593ca178e815880874277b55d3
suminhan123 <suminh324@gmail.com>
2026-02-17 19:07:35 +0900

    feat: 카테고리 title ui 개발

--- apps/extension/src/features/analysis/components/AnalysisView.tsx
+++ apps/extension/src/features/analysis/components/AnalysisView.tsx
@@ -1,12 +1,19 @@
 import DateRangePicker from "@/components/DateRangePicker";
+import Divider from "@/components/Divider";
+import CategoryAnalysisSection from "@/features/analysis/components/category-analysis/CategoryAnalysisSection";
+import TodayTimeThiefSection from "@/features/analysis/components/TodayTimeThiefSection";
 import WeeklyScreenTimeSection from "@/features/analysis/components/WeeklyScreenTimeSection";
 
 const AnalysisView = () => {
   return (
-    <div>
+    <>
       <DateRangePicker />
       <WeeklyScreenTimeSection />
-    </div>
+      <Divider />
+      <CategoryAnalysisSection />
+      <Divider />
+      <TodayTimeThiefSection />
+    </>
   );
 };
 
