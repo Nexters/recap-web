9c6e20b8cf14db2a02e86764661c124175ebcc15
suminhan123 <suminh324@gmail.com>
2026-02-17 21:39:41 +0900

    feat: button 컴포넌트 추가

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 
-const button = () => {
-  return <div>button</div>;
-};
+import { cn } from "./utils/cn";
 
-export default button;
+const buttonVariants = cva(
+  "w-full items-center cursor-pointer justify-center whitespace-nowrap rounded-xl transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "bg-gray-800 text-white hover:bg-gray-700",
+        secondary: "bg-gray-100 text-gray-900 hover:bg-gray-200 ",
+      },
+      size: {
+        sm: "py-1.5 text-label-2",
+        md: "py-4 text-subtitle-1-md",
+        lg: "py-5 text-subtitle-2-sb",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "md",
+    },
+  },
+);
+
+export interface ButtonProps
+  extends
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+function Button({ className, variant, size, ...props }: ButtonProps) {
+  return (
+    <button
+      className={cn(buttonVariants({ variant, size }), className)}
+      {...props}
+    />
+  );
+}
+
+export { Button, buttonVariants };
