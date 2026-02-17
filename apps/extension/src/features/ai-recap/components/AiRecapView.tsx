fd90f0a31c7c0cdae12f57497084c038db98e46b
suminhan123 <suminh324@gmail.com>
2026-02-17 20:58:20 +0900

    feat: 오늘 Recap UI 개발

--- apps/extension/src/features/ai-recap/components/AiRecapView.tsx
+++ apps/extension/src/features/ai-recap/components/AiRecapView.tsx
@@ -1,8 +1,14 @@
+import Divider from "@/components/Divider";
+import TodayRecapDetail from "@/features/ai-recap/components/TodayRecapDetail";
+import TodayRecapSection from "@/features/ai-recap/components/TodayRecapSection";
+
 const AiRecapView = () => {
   return (
-    <div>
-      <h1>AiRecapView</h1>
-    </div>
+    <>
+      <TodayRecapSection />
+      <TodayRecapDetail />
+      <Divider />
+    </>
   );
 };
 
