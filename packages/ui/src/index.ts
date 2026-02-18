b9d8c653adc443a12a6da6153cb6214dae6acc74
suminhan123 <suminh324@gmail.com>
2026-02-18 19:43:47 +0900

    feat: input ui 컴포넌트 추가

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./accordion";
 export * from "./badge";
 export * from "./button";
+export * from "./input";
 export * from "./switch";
 export * from "./tabs";
 export * from "./toggle-group";
